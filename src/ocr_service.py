ea3ea5e7d7f4fbcdbb9eaa01f8200b339a7b0206
TuanSimonetti <tuansimonetti@hotmail.com>
2024-03-18 15:05:13 -0700

    Add parametry to process pdf with digital signatures

--- src/ocr_service.py
+++ src/ocr_service.py
@@ -7,4 +7,4 @@ def pdf_ocr(input_directory, output_directory, pdf_file):
         file = os.listdir(input_directory)
         if pdf_file in file:
             break
-    ocrmypdf.ocr(f'{input_directory}/{pdf_file}', f'{output_directory}/{pdf_file}', deskew=True, force_ocr=True)
+    ocrmypdf.ocr(f'{input_directory}/{pdf_file}', f'{output_directory}/{pdf_file}', deskew=True, force_ocr=True, invalidate_digital_signatures=True)
